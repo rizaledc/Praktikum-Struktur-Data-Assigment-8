bc86e2d406b2a1620a0b91d46a985ccbab59e7e0
Rizal Wahyu Pratama <161397994+rizaledc@users.noreply.github.com>
2024-05-22 03:59:34 +0700

    .

--- Modul 8/uguided1.cpp
+++ Modul 8/uguided1.cpp
@@ -2,21 +2,27 @@
 #include <algorithm>
 #include <vector>
 
+// Mendeklarasikan vector untuk menyimpan elemen heap
 std::vector<int> H;
+// Mendeklarasikan variabel untuk menyimpan ukuran heap saat ini
 int heapSize = -1;
 
+// Fungsi untuk mendapatkan indeks parent dari node saat ini
 int parent(int i) {
     return (i - 1) / 2;
 }
 
+// Fungsi untuk mendapatkan indeks anak kiri dari node saat ini
 int leftChild(int i) {
     return (2 * i) + 1;
 }
 
+// Fungsi untuk mendapatkan indeks anak kanan dari node saat ini
 int rightChild(int i) {
     return (2 * i) + 2;
 }
 
+// Fungsi untuk memperbaiki posisi node ke atas heap jika diperlukan
 void shiftUp(int i) {
     while (i > 0 && H[parent(i)] < H[i]) {
         std::swap(H[parent(i)], H[i]);
@@ -24,6 +30,7 @@ void shiftUp(int i) {
     }
 }
 
+// Fungsi untuk memperbaiki posisi node ke bawah heap jika diperlukan
 void shiftDown(int i) {
     int maxIndex = i;
     int l = leftChild(i);
@@ -41,6 +48,7 @@ void shiftDown(int i) {
     }
 }
 
+// Fungsi untuk memasukkan elemen baru ke dalam heap
 void insert(int p) {
     if (static_cast<size_t>(heapSize + 1) >= H.size()) {
         H.push_back(p); // Memastikan ada ruang di vector sebelum menambahkan elemen
@@ -50,6 +58,8 @@ void insert(int p) {
     heapSize++;
     shiftUp(heapSize);
 }
+
+// Fungsi untuk mengambil elemen maksimum dari heap dan memperbaiki struktur heap
 int extractMax() {
     int result = H[0];
     H[0] = H[heapSize];
@@ -58,6 +68,7 @@ int extractMax() {
     return result;
 }
 
+// Fungsi untuk mengubah prioritas elemen di heap dan memperbaiki struktur heap
 void changePriority(int i, int p) {
     int oldp = H[i];
     H[i] = p;
@@ -68,16 +79,19 @@ void changePriority(int i, int p) {
     }
 }
 
+// Fungsi untuk mendapatkan elemen maksimum dari heap tanpa menghapusnya
 int getMax() {
     return H[0];
 }
 
+// Fungsi untuk menghapus elemen dari heap
 void remove(int i) {
     H[i] = getMax() + 1;
     shiftUp(i);
     extractMax();
 }
 
+// Fungsi utama untuk menjalankan operasi heap
 int main() {
     int numElements, element;
     std::cout << "Masukkan jumlah elemen: ";
